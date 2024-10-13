cfad4be4318a48266e2929281600db846b274f3d
greatbrain <erickfania@gmail.com>
2024-10-13 08:06:51 -0400

    add:defaultdict for grouping data by days

--- app/main.py
+++ app/main.py
@@ -4,15 +4,16 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from db.database import Base, engine, get_db
 from models.activity import ActivityDataModel
-
-
 from fastapi import HTTPException, status, Depends
 from datetime import datetime
 from schemas.activity_schema import ActivityCreate, ActivityResponse
+from collections import defaultdict
 
 Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
+# Agrupa los servicios por dias, en un diccionario de listas
+activities_default_dict = defaultdict(list)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
