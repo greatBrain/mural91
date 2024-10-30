cb716e2ccca90a8069746afb97bed866f903dd36
greatbrain <erickfania@gmail.com>
2024-10-30 00:18:28 -0400

    add: separation of cards by days

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -20,6 +20,7 @@ document.addEventListener("DOMContentLoaded", function () {
                         // Crea un nuevo div para cada tarjeta de actividad
                         let cardItem = document.createElement("div");
                         cardItem.classList.add('card-item', `card-${day}`); // Usar comillas invertidas para interpolación
+                        cardItem.setAttribute("data-day", day);
 
                         // Crea el contenedor para el título de la actividad
                         let activityTitle = document.createElement('div');
@@ -47,6 +48,28 @@ document.addEventListener("DOMContentLoaded", function () {
                     console.log(`Contenido de activities[${day}]:`, activities[day]); // Muestra qué hay en activities[day]
                 }
             }
+
+            // Hide all activities cards at first
+            const allCardsItems = document.querySelector(".card-item");
+
+            if (allCardsItems) {
+                allCardsItems.forEach(card => card.style.display = "none");
+            }
+
+            // Event listener
+            const daysList = document.querySelectorAll(".days-list li");
+            daysList.forEach(dayItem => {
+                dayItem.addEventListener("click", function () {
+                    const selectedDay = dayItem.getAttribute("data-day");
+
+                    // Oculta todas las tarjetas primero
+                    allCards.forEach(card => card.style.display = "none");
+
+                    // Muestra las tarjetas correspondientes al día seleccionado
+                    const dayCards = document.querySelectorAll(`.card-item[data-day="${selectedDay}"]`);
+                    dayCards.forEach(card => card.style.display = "block");
+                });
+            });
         })
         .catch(error => {
             console.error('Hubo un problema con la solicitud Fetch:', error);
